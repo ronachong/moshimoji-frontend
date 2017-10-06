838c40dab4fb1f96be655dc00b64b65e1f991b83
Rona Chong <ronachong@gmail.com>
2017-10-06 15:50:26 -0700

    Add new model for Status component; test.

--- docker-backend/graphene-django-backend/project/example_app/admin.py
+++ docker-backend/graphene-django-backend/project/example_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import MangaSeries, Genre, Author
+from .models import Status, MangaSeries, Genre, Author
 
+admin.site.register(Status)
 admin.site.register(MangaSeries)
 admin.site.register(Genre)
 admin.site.register(Author)
--- docker-backend/graphene-django-backend/project/example_app/models.py
+++ docker-backend/graphene-django-backend/project/example_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Status(models.Model):
+    user = models.ForeignKey('auth.User')
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
 class Genre(models.Model):
     name = models.CharField(max_length=100)
 
