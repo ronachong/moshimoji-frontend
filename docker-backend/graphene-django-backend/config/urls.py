9e7ec937f99d1821cbf746e72c4ce37467267065
Rona Chong <ronachong@gmail.com>
2017-10-09 21:38:13 -0700

    Update with urls for jwt related views; update also urls for graphql things.

--- docker-backend/graphene-django-backend/config/urls.py
+++ docker-backend/graphene-django-backend/config/urls.py
@@ -15,12 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
+
 from project.example_app.graphene.schema import schema
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', GraphQLView.as_view(graphiql=True, schema=schema)),
+    url(r'^graphiql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    url(r'^gql', csrf_exempt(GraphQLView.as_view(batch=True))),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
     # url(r'^', include('project.example_app.urls')),
 ]
