844c926a313640544b3258f9e8012dcb3acee5ee
Rona Chong <ronachong@gmail.com>
2017-10-15 14:43:47 -0700

    Convert DashboardLinkOrButton to class component.
    As opposed to stateless functional component. This conversion is motivated by the ability to use the @connect decorator when I want to hook up the button to the login modal state via Redux.

--- reactql-frontend/src/components/main/index/DashboardLinkOrButton.js
+++ reactql-frontend/src/components/main/index/DashboardLinkOrButton.js
@@ -1,35 +1,58 @@
 import React from 'react';
 import LinkOrButton from 'src/components/reused/LinkOrButton';
 
-const DashboardLinkOrButton = ({ currentUser }) => {
-  // TODO: consider refactoring this and LinkOrButton; does propsToPass really make sense?
-  // or can I simply just pass uri, onclick separately? should the isLink logic be moved elswhere?
-  const properties = (currentUser) ?
-    {
-      isLink: true,
-      propsToPass: {
-        uri: '/dashboard/site',
-      },
-    } :
-    {
-      isLink: false,
-      propsToPass: {
-        onClick: () => {
-          console.log('click 2');
+// TODO: figure out if this can/should be rewritten as a stateless functional
+// component, even if it needs the the dispatch prop (don't think I can use an
+// es6 class decorator with a functional component, so can't use @connect)
+// TODO: consider refactoring this and LinkOrButton;
+// does it make sense to store things in this.state if they aren't changing vals?
+// does propsToPass really make sense?
+// or can I simply just pass uri, onclick separately?
+// should the isLink logic be moved elswhere?
+class DashboardLinkOrButton extends React.PureComponent {
+  static propTypes = {
+    // counter: PropTypes.shape({
+    //   count: PropTypes.number.isRequired,
+    // }),
+  };
+
+  static defaultProps = {
+    // counter: {
+    //   count: 0,
+    // },
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = (this.props.currentUser) ?
+      {
+        isLink: true,
+        propsToPass: {
+          uri: '/dashboard/site',
         },
-      },
-    };
+      } :
+      {
+        isLink: false,
+        propsToPass: {
+          onClick: () => {
+            console.log('dashboard button was clicked');
+          },
+        },
+      };
+  }
 
-  const DisplayComponent = ({ onClick }) => (
-    <button onClick={onClick}>dashboard</button>
-  );
+  render() {
+    const DisplayComponent = ({ onClick }) => (
+      <button onClick={onClick}>dashboard</button>
+    );
 
-  return (
-    <LinkOrButton
-      DisplayComponent={DisplayComponent}
-      isLink={properties.isLink}
-      propsToPass={properties.propsToPass} />
-  );
-};
+    return (
+      <LinkOrButton
+        DisplayComponent={DisplayComponent}
+        isLink={this.state.isLink}
+        propsToPass={this.state.propsToPass} />
+    );
+  }
+}
 
 export default DashboardLinkOrButton;
