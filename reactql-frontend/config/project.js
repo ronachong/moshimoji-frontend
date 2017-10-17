e1e7e21967e5384b4eb0a784479ff657175ddd40
Rona Chong <ronachong@gmail.com>
2017-10-16 17:27:54 -0700

    Add attribute for uri to retrieve jwt to config class.

--- reactql-frontend/config/project.js
+++ reactql-frontend/config/project.js
@@ -6,6 +6,11 @@ import config from 'kit/config';
 // ----------------------
 // PROJECT CONFIGURATION
 
+// eslint-disable-next-line no-console
+console.log('project config being run');
+// eslint-disable-next-line no-console
+console.log(process.env);
+
 /* BACKEND */
 const uriOptions = {
   servers: {
@@ -18,19 +23,19 @@ const uriOptions = {
   },
 };
 
-// eslint-disable-next-line no-console
-console.log('project config being run');
-// eslint-disable-next-line no-console
-console.log(process.env);
-
 /* GRAPHQL */
-// TODO: add logic to determine graphql endpoint in docker for aws swarm
+// TODO: add logic to determine graphql,jwt endpoint in docker for aws swarm
 if (process.env.NODE_ENV === 'development') {
   config.setGraphQLEndpoint(
     `http://${uriOptions.servers.development}/${uriOptions.slugs.graphql}`
   );
+  config.setJwtEndpoint(
+    `http://${uriOptions.servers.development}/${uriOptions.slugs.jwtRetrieve}`
+  );
   // eslint-disable-next-line no-console
   console.log(`set graphql endpoint to ${config.graphQLEndpoint} in project config`);
+  // eslint-disable-next-line no-console
+  console.log(`set endpoint to retrieve jwt to ${config.jwtEndpoint} in project config`);
 }
 
 /* APOLLO */
