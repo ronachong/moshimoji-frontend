8c130b2f693adda08d8fcc7b1c76903571bd0f25
Rona Chong <ronachong@gmail.com>
2017-10-17 18:36:46 -0700

    Reduce repo contents to just frontend.

--- docker-backend/graphene-django-backend/project/gql_platform/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import UserStatus, MangaSeries, Genre, Author
-
-admin.site.register(UserStatus)
-admin.site.register(MangaSeries)
-admin.site.register(Genre)
-admin.site.register(Author)
