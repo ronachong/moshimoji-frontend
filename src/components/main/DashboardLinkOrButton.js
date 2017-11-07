013122a02b0c19e85e4a543fe816dc267ef600f7
Rona Chong <ronachong@gmail.com>
2017-11-07 15:28:01 -0800

    Add some documentation to DashboardLinkOrButton.

--- src/components/main/DashboardLinkOrButton.js
+++ src/components/main/DashboardLinkOrButton.js
@@ -16,12 +16,16 @@ import { toggleLoginModal } from 'src/store/actions';
 
 // ----------------------
 // COMPONENT
+// DashboardLinkOrButton is an HOC around LinkOrButton. It specifies
+// the display component (dashboard button) and dictates isLink or
+// button based on the currentUser prop.
+
 // TODO: figure out if this I should rewrite this as a stateless functional cmp
 // TODO: consider refactoring this and LinkOrButton;
-// does it make sense to store things as class attrs?
-// does propsToPass really make sense?
-// or can I simply just pass uri, onclick separately?
-// should the isLink logic be moved elswhere?
+// TODO: determine if it make sense to store things as class attrs instead of state
+// TODO: determine if propsToPass really makes sense, or if I can/should pass uri,
+// onClick, separately
+// TODO: figure out if isLink logic should be moved elswhere
 @connect()
 class DashboardLinkOrButton extends React.PureComponent {
   static propTypes = {
