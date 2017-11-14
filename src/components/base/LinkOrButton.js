ce5ece7ed944e59ad7eec1b11de33585fd808db6
Rona Chong <ronachong@gmail.com>
2017-11-13 18:29:34 -0800

    Add documentation to LinkOrButton.

--- src/components/base/LinkOrButton.js
+++ src/components/base/LinkOrButton.js
@@ -6,6 +6,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
+// ----------------------
+// COMPONENT: LinkOrButton
+// LinkOrButton is a base component that behaves either as a link or a button
+// depending on isLink prop.
 const LinkOrButton = ({ DisplayComponent, isLink, onClick, uri }) => {
   if (isLink) {
     return <Link to={uri}><DisplayComponent /></Link>;
