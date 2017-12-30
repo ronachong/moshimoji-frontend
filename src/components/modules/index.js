5f7845f56c015800c49163f0141cbf534883dd4c
Rona Chong <ronachong@gmail.com>
2017-12-30 14:50:35 -0800

    Update module index.

--- src/components/modules/index.js
+++ src/components/modules/index.js
@@ -1,8 +1,8 @@
-import Dashboard from 'src/components/modules/dashboard/ApolloDashboard';
+import Dashboard from 'src/components/modules/dashboard';
 import Database from 'src/components/modules/Database';
 import Doujin from 'src/components/modules/Doujin';
 import Forum from 'src/components/modules/Forum';
-import Reader from 'src/components/modules/Reader';
+import Reader from 'src/components/modules/reader';
 import Reviews from 'src/components/modules/Reviews';
 import SiteNews from 'src/components/modules/SiteNews';
 
