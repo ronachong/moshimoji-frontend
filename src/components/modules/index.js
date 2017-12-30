52d95ed9846f500bf21a67bb3cd74f3d7c0ec6de
Rona Chong <ronachong@gmail.com>
2017-12-30 14:50:35 -0800

    Update module index.

--- src/components/modules/index.js
+++ src/components/modules/index.js
@@ -2,7 +2,7 @@ import Dashboard from 'src/components/modules/dashboard';
 import Database from 'src/components/modules/Database';
 import Doujin from 'src/components/modules/Doujin';
 import Forum from 'src/components/modules/Forum';
-import Reader from 'src/components/modules/Reader';
+import Reader from 'src/components/modules/reader';
 import Reviews from 'src/components/modules/Reviews';
 import SiteNews from 'src/components/modules/SiteNews';
 
