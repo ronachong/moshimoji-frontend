afc6d3b7e2c8b88e8cc9c3fceff5886c582226f8
Rona Chong <ronachong@gmail.com>
2018-02-21 14:32:28 -0800

    Implement basic example of react bootstrap modal.
    see: https://react-bootstrap.github.io/components/modal/
    
    When this modal component has animation=true passed to it, the
    modal elements are rendered with opacity=0 under the .fade class.
    When animation=false, the elements show up.
    
    (description in travails log: https://paper.dropbox.com/doc/travailsvictory-log-in-dev-stuff-urw9F3oeUadSzjULXWYiM#:h2=[now-2:15-pm])
    
    Why is the animation process failing to work as intended?

--- src/components/main/LoginModal.js
+++ src/components/main/LoginModal.js
@@ -4,45 +4,12 @@
 /* NPM */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-// HOC/decorator to listen to Redux store state
 import { connect } from 'react-redux';
-
 import { Modal, Transition } from 'react-bootstrap';
 
+/* Moshimoji */
 import config from 'kit/config';
 import { showLoginModal } from 'src/store/actions';
-
-// ----------------------
-// STYLING
-const FADE_DURATION = 200;
-
-const styles = {
-  modal : {
-    position: 'fixed',
-    zIndex: 1040,
-    top: 0, bottom: 0, left: 0, right: 0,
-  },
-  modalBackdrop : {
-    position: 'fixed',
-    top: 0, bottom: 0, left: 0, right: 0,
-    zIndex: 'auto',
-    backgroundColor:'#000',
-    opacity: 0.5
-  },
-  modalTextContainer : {
-    position: 'absolute',
-    width: '50%',
-    height: '50%',
-    top: '50%', left: '50%',
-    transform: `translate(-${50}%, -${50}%)`,
-    border: '1px solid #fff555',
-    backgroundColor: 'white',
-    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
-    padding: 20,
-    textAlign: 'center'
-  }
-};
 
 // -----------------------
 // REDUX
@@ -90,37 +57,44 @@ class LoginModal extends Component {
     // of modal (dashboard button or link preceding)
     const loginDest = '/dashboard'
     return (
-      // <Transition
-      //   in={true}
-      //   timeout={FADE_DURATION}
-      //   className='fade'
-      //   enteredClassName='in'
-      //   enteringClassName='in'>
-        <Modal
-          show={this.props.loginModal.show}
-          onHide={() => {this.props.dispatch(showLoginModal(false))}}
-          style={styles.modal}
-          backdropStyle={styles.modalBackdrop}>
-          <Modal.Body style={styles.modalTextContainer}>
-            <form
-              ref={ref => (this.form = ref)}
-              onSubmit={e => this.handleSubmit(loginDest, e)}
-            >
-              <div>
-                <label>Username:</label>
-                <input type="text" name="slug" />
-              </div>
-              <div>
-                <label>Password:</label>
-                <input type="password" name="password" />
-              </div>
-              <button type="submit">Login</button>
-            </form>
-          </Modal.Body>
-        </Modal>
-      // </Transition>
+      <Modal
+        show={this.props.loginModal.show}
+        onHide={() => {this.props.dispatch(showLoginModal(false))}}
+        animation={true}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>H4 header</h4>
+          <p>Paragraph text.</p>
+        </Modal.Body>
+      </Modal>
     );
   }
 }
 
 export default LoginModal;
+
+// <Modal
+//   show={this.props.loginModal.show}
+//   onHide={() => {this.props.dispatch(showLoginModal(false))}}
+//   style={styles.modal}
+//   backdropStyle={styles.modalBackdrop}>
+//     <Modal.Body style={styles.modalTextContainer}>
+//       <form
+//         ref={ref => (this.form = ref)}
+//         onSubmit={e => this.handleSubmit(loginDest, e)}
+//       >
+//         <div>
+//           <label>Username:</label>
+//           <input type="text" name="slug" />
+//         </div>
+//         <div>
+//           <label>Password:</label>
+//           <input type="password" name="password" />
+//         </div>
+//         <button type="submit">Login</button>
+//       </form>
+//     </Modal.Body>
+// </Modal>
